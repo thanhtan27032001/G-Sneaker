5bd29a72eff1354c4e94c0241038d3cf069993b8
ThanhTan <thanhtan27032001@gmail.com>
2024-02-27 16:55:08 +0700

    clear autogen code and define colors

---
+++ lib/app/config/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static final Color _whiteColor = Color(int.parse("0xffffffff"));
+
+  static final Color _blackColor = Color(int.parse("0xff303841"));
+
+  static final Color _grayColor = Color(int.parse("0xff777777"));
+
+  static final Color _yellowColor = Color(int.parse("0xfff6C90e"));
+
+  static Color blackColor() => _blackColor;
+
+  static Color whiteColor() => _whiteColor;
+
+  static Color grayColor() => _grayColor;
+
+  static Color yellowColor() => _yellowColor;
+}
