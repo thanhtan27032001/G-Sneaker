ec73d27fa773920b0e4744c9e4abbcec4336d0d3
ThanhTan <thanhtan27032001@gmail.com>
2024-02-28 10:15:36 +0700

    connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
 import 'app/pages/main/main_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDqWdp7F40u8I9W_8nhsO1O6wV80o2m6S0",
+          projectId: "gshoe-f83eb",
+          messagingSenderId: "714198316325",
+          appId: "1:714198316325:web:65a13810f5f3c7f659d6cc",
+      ));
   runApp(const MyApp());
 }
 
@@ -14,20 +23,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      debugShowCheckedModeBanner: false,
-      initialRoute: "main",
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        debugShowCheckedModeBanner: false,
+        initialRoute: "main",
         getPages: [
-          GetPage(
-              name: '/main',
-              page: () => MainPage()),
-        ]
-    );
+          GetPage(name: '/main', page: () => MainPage()),
+        ]);
   }
 }
 
